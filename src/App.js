ba77b429ae8e79e64a3caf2d156aa2d2c897bfde
Kaushal Rijal <64598295+kaushalrijal@users.noreply.github.com>
2022-07-15 18:52:55 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Home from './components/Home';
 import Navbar from './components/Navbar';
 import About from './components/About';
 import Skills from './components/Skills';
-import Work from './components/Work';
 import Contact from './components/Contact';
 
 function App() {
@@ -13,7 +12,6 @@ function App() {
       <Home />
       <About />
       <Skills />
-      <Work />
       <Contact />
     </React.Fragment>
   );
