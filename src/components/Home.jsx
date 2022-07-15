ef97465a00b2fce5c22e278edef3347823d5ae6e
Kaushal Rijal <64598295+kaushalrijal@users.noreply.github.com>
2022-07-15 18:56:05 +0545

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
           Kaushal Rijal
         </h1>
         <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
-          You are Jasmine but (Jas) is silent.
+          You know I&apos;m something of a programmer myself.
         </h2>
         <p className="text-[#8892b0] py-4 max-w-[700px] text-justify">
           I love to code and I have been coding since I was 12. My cousin first
@@ -22,9 +22,9 @@ const Home = () => {
           yet and you might not be interested.
         </p>
         <div>
-          <Link to="work" smooth={true} duration={500}>
+          <Link to="about" smooth={true} duration={500}>
             <button className="text-white group border-2 px-4 py-2 my-2 flex items-center hover:bg-[#8C8C8C] hover:border-[#8c8c8c]">
-              View Work{" "}
+              Read More{" "}
               <span className="group-hover:rotate-90 duration-300">
                 <HiArrowNarrowRight className="ml-4" />
               </span>
