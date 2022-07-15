b6752bc964ac7d67b92f13ece0acdb1fe1e59042
Kaushal Rijal <64598295+kaushalrijal@users.noreply.github.com>
2022-07-15 18:32:29 +0545

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
-import { BsFillPersonLinesFill } from "react-icons/bs";
 import Logo from "../img/logo_dark.png";
 import { Link } from "react-scroll";
 
