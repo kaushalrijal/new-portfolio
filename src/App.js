35eb1f1a084dfd9e76d1263fe3a83e11e2c0dcac
Kaushal Rijal <64598295+kaushalrijal@users.noreply.github.com>
2022-07-18 13:21:33 +0545

    few changes made

--- src/App.js
+++ src/App.js
@@ -5,16 +5,24 @@ import About from './components/About';
 import Skills from './components/Skills';
 import Work from './components/Work';
 import Contact from './components/Contact';
+import { Routes, Route } from "react-router-dom"
 
 function App() {
   return (
     <React.Fragment>
       <Navbar />
-      <Home />
-      <About />
-      <Skills />
-      <Work />
-      <Contact />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/work" element={<Work />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      {/* <Home />
+        <About />
+        <Skills />
+        <Work />
+        <Contact /> */}
     </React.Fragment>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,8 @@ import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import Logo from "../img/logo_dark.png";
-import { Link } from "react-scroll";
+//import { Link } from "react-scroll";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -18,29 +19,19 @@ const Navbar = () => {
       {/* Menu */}
       <ul className="hidden md:flex">
         <li>
-          <Link to="home" smooth={true} duration={500}>
-            Home
-          </Link>
+          <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to="about" smooth={true} duration={500}>
-            About
-          </Link>
+          <Link to="about">About</Link>
         </li>
         <li>
-          <Link to="skills" smooth={true} duration={500}>
-            Skills
-          </Link>
+          <Link to="skills">Skills</Link>
         </li>
         <li>
-          <Link to="work" smooth={true} duration={500}>
-            Work
-          </Link>
+          <Link to="work">Work</Link>
         </li>
         <li>
-          <Link to="contact" smooth={true} duration={500}>
-            Contact
-          </Link>
+          <Link to="contact">Contact</Link>
         </li>
       </ul>
 
@@ -58,27 +49,27 @@ const Navbar = () => {
         }
       >
         <li className="py-6 text-4xl">
-          <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+          <Link onClick={handleClick} to="home">
             Home
           </Link>
         </li>
         <li className="py-6 text-4xl">
-          <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+          <Link onClick={handleClick} to="about">
             About
           </Link>
         </li>
         <li className="py-6 text-4xl">
-          <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
+          <Link onClick={handleClick} to="skills">
             Skills
           </Link>
         </li>
         <li className="py-6 text-4xl">
-          <Link onClick={handleClick} to="work" smooth={true} duration={500}>
+          <Link onClick={handleClick} to="work">
             Work
           </Link>
         </li>
         <li className="py-6 text-4xl">
-          <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+          <Link onClick={handleClick} to="contact">
             Contact
           </Link>
         </li>
